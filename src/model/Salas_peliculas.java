fa9d5c45646e8b50ba56ede884286483bbcdddfc
samuel <samuel1122000@gmail.com>
2025-04-29 19:31:54 +0200

    Intentar automatizas salas pelis

--- src/model/Salas_peliculas.java
+++ src/model/Salas_peliculas.java
@@ -3,61 +3,53 @@ package model;
 import java.sql.Date;
 import java.sql.Time;
 
-public class Salas_peliculas{
+public class Salas_peliculas {
 
     private int id;
-    private String nombre;
-    private int duracion;
     private Date fecha_inicio_emision;
     private Date fecha_fin_emision;
     private Time hora_inicio;
     private Time hora_fin;
     private int id_sala;
     private int id_pelicula;
-    private int id_entrada;
 
-    public Salas_peliculas(){}
+    public Salas_peliculas() {}
 
-    public Salas_peliculas(String nombre,int duracion, Date fecha_inicio_emision, Date fecha_fin_emision){
-        this.nombre = nombre;
-        this.duracion = duracion;
+    public Salas_peliculas(Date fecha_inicio_emision, Date fecha_fin_emision,Time hora_inicio, Time hora_fin,int id_sala, int id_pelicula) {
         this.fecha_inicio_emision = fecha_inicio_emision;
         this.fecha_fin_emision = fecha_fin_emision;
+        this.hora_inicio = hora_inicio;
+        this.hora_fin = hora_fin;
+        this.id_sala = id_sala;
+        this.id_pelicula = id_pelicula;
     }
 
-    public int getId(){ return id ; }
-    public String getNombre(){ return nombre; }
-    public int getDuracion(){ return duracion; }
-    public Time getHora_inicio(){ return hora_inicio ; }
-    public Time getHora_fin(){ return hora_fin; }
-    public int getId_sala(){ return id_sala; }
-    public int getId_pelicula(){ return id_pelicula; }
-    public int getId_entrada(){ return id_entrada; }
+    public int getId() { return id; }
+    public Date getFecha_inicio_emision() { return fecha_inicio_emision; }
+    public Date getFecha_fin_emision() { return fecha_fin_emision; }
+    public Time getHora_inicio() { return hora_inicio; }
+    public Time getHora_fin() { return hora_fin; }
+    public int getId_sala() { return id_sala; }
+    public int getId_pelicula() { return id_pelicula; }
 
-    public void setId(int id){ this.id = id; }
-    public void setNombre(String nombre){ this.nombre = nombre; }
-    public void setDuracion(int duracion){ this.duracion = duracion; }
-    public void setFecha_inicio_emision(Date fecha_inicio_emision){ this.fecha_inicio_emision = fecha_inicio_emision; }
-    public void setFecha_fin_emision(Date fecha_fin_emision){ this.fecha_fin_emision = fecha_fin_emision; }
-    public void setHora_inicio(Time hora_inicio){ this.hora_inicio = hora_inicio ; }
-    public void setHora_fin(Time hora_fin){ this.hora_fin = hora_fin; }
-    public void setId_sala(int id_sala){ this.id_sala = id_sala ;}
-    public void setId_pelicula(int id_pelicula){ this.id_pelicula = id_pelicula ;}
-    public void setId_entrada(int id_entrada){ this.id_entrada = id_entrada ;}
+    public void setId(int id) { this.id = id; }
+    public void setFecha_inicio_emision(Date fecha_inicio_emision) { this.fecha_inicio_emision = fecha_inicio_emision; }
+    public void setFecha_fin_emision(Date fecha_fin_emision) { this.fecha_fin_emision = fecha_fin_emision; }
+    public void setHora_inicio(Time hora_inicio) { this.hora_inicio = hora_inicio; }
+    public void setHora_fin(Time hora_fin) { this.hora_fin = hora_fin; }
+    public void setId_sala(int id_sala) { this.id_sala = id_sala; }
+    public void setId_pelicula(int id_pelicula) { this.id_pelicula = id_pelicula; }
 
     @Override
-    public String toString(){ //cambiar hora inicio y fin ya que es automatico
+    public String toString() {
         return "Salas_peliculas{" +
-        "id = "+ id +
-        ", nombre =" + nombre + '\'' +
-        ", fecha_inicio_emision =" + fecha_inicio_emision + '\'' +
-        ", fecha_fin_emision =" + fecha_fin_emision + '\'' +
-        ", hora_inicio = " + hora_inicio+ '\'' +
-        ", hora_fin =" + hora_fin+ '\'' +
-        ", Id_sala =" + id_sala + '\'' +
-        ", Id_pelicula =" + id_pelicula+ '\'' +
-        ", Id_entrada =" + id_entrada + '\'' +
-        "}";
+                "id=" + id +
+                ", fecha_inicio_emision=" + fecha_inicio_emision +
+                ", fecha_fin_emision=" + fecha_fin_emision +
+                ", hora_inicio=" + hora_inicio +
+                ", hora_fin=" + hora_fin +
+                ", id_sala=" + id_sala +
+                ", id_pelicula=" + id_pelicula +
+                '}';
     }
-
 }
--- src/view/Salas_peliculasView.java
+++ src/view/Salas_peliculasView.java
@@ -13,19 +13,22 @@ import model.Emision;
 import dao.Salas_peliculasDao;
 import dao.EmisionDAO;
 import dao.SalasDao;
+import dao.PeliculaDAO;
 
 public class Salas_peliculasView {
 
     private final Scanner sc = new Scanner(System.in);
     private final Salas_peliculasDao dao = new Salas_peliculasDao();
     private final EmisionDAO emisionDao = new EmisionDAO();
+    private final SalasDao salasDao = new SalasDao();
+    private final PeliculaDAO peliculaDAO = new PeliculaDAO();
 
     public void gestionSalas_Peliculas(){
         int opcion = 0;
         do { 
             System.out.println(" \n Gestion de Salas y peliculas");
-            System.out.println("1. Establecer emision de una pelicula");
-            System.out.println("2. Automatizar gestion de salas y peliculas");
+            System.out.println("1. Establecer emisiones para peliculas");
+            System.out.println("2. ");
             System.out.println("3. Eliminar salas y peliculas ");
             System.out.println("4. Mostrar salas y peliculas disponibles");
             System.out.println("5. Buscar una sala y pelicula por ID");
@@ -48,57 +51,76 @@ public class Salas_peliculasView {
     }
 
     public void emisionPelicula(){
-        System.out.println("Nombre de la pelicula: ");
-        String nombre = sc.nextLine();
-        System.out.println("Duracion: ");
-        int duracion = Integer.parseInt(sc.nextLine());
-        System.out.println("Fecha de inico de la emision (YYYY-MM-DD): ");
-        String fechaInicio = sc.nextLine();
-        LocalDate localDateInicio = LocalDate.parse(fechaInicio, DateTimeFormatter.ISO_LOCAL_DATE);
-        Date fecha_inicio_emision = Date.valueOf(localDateInicio); 
-        System.out.println("Fecha de fin de la emision (YYYY-MM-DD): ");
-        String fechaFin = sc.nextLine();
-        LocalDate localDateFin = LocalDate.parse(fechaFin, DateTimeFormatter.ISO_LOCAL_DATE);
-        Date fecha_fin_emision = Date.valueOf(localDateFin); 
+        String respuesta = "si";
+        do{
+            System.out.println("Nombre de la película:");
+            String nombre = sc.nextLine();
 
-        Emision emision = new Emision(nombre, duracion, fecha_inicio_emision, fecha_fin_emision );
-        emisionDao.agregarEmision(emision);
-        System.out.println("Emision agregada correctamente ");
+            int idPelicula = peliculaDAO.obtenerIdPeliculaPorNombre(nombre); // Falta de implementar para obtener el ID 
         
+            if (idPelicula == -1) {
+                System.out.println(" Película no encontrada. Primero debes añadirla al sistema.");
+                return;
+            }
+            System.out.println("Duracion (minutos): ");
+            int duracion = Integer.parseInt(sc.nextLine());
+            System.out.println("Fecha de inico de la emision (YYYY-MM-DD): ");
+            String fechaInicio = sc.nextLine();
+            LocalDate localDateInicio = LocalDate.parse(fechaInicio, DateTimeFormatter.ISO_LOCAL_DATE);
+            Date fecha_inicio_emision = Date.valueOf(localDateInicio); 
+            System.out.println("Fecha de fin de la emision (YYYY-MM-DD): ");
+            String fechaFin = sc.nextLine();
+            LocalDate localDateFin = LocalDate.parse(fechaFin, DateTimeFormatter.ISO_LOCAL_DATE);
+            Date fecha_fin_emision = Date.valueOf(localDateFin); 
+
+            Emision emision = new Emision(nombre, duracion, fecha_inicio_emision, fecha_fin_emision, idPelicula );
+            emisionDao.agregarEmision(emision);
+            System.out.println("Emision agregada correctamente ");
+            System.out.println("                               ");
+            System.out.println("Desea seguir añadiendo peliculas");
+            respuesta = sc.nextLine();
+
+        } while(respuesta.equals("si"));
+
+        gestionAutomaticaSalasyPeliculas();//????
     }
 
-    public void gestionAutomaticaSalasyPeliculas(){ //Hacer aqui el algoritmo que asigne automaticamente todo y esto sera lo que se inserte en la Base de datos salas_peliculas
+    //En este metodo se gestiona de forma automatica las salas y peliculas y se insertan en la BD y en salas_peliculas 
+    public void gestionAutomaticaSalasyPeliculas() { 
         ArrayList<Emision> emisiones = emisionDao.mostrarEmision();
         ArrayList<Salas> salas = dao.obtenerSala();
-
-        for(Emision em : emisiones){
+    
+        for (Emision em : emisiones) {
             LocalDate inicio = em.getFecha_inicio_emision().toLocalDate();
             LocalDate fin = em.getFecha_fin_emision().toLocalDate();
-            int duracion = em.getDuracion(); // en minutos
-
+            int duracion = em.getDuracion();
+            int idPelicula = em.getIdPelicula();
+    
             for (LocalDate fecha = inicio; !fecha.isAfter(fin); fecha = fecha.plusDays(1)) {
                 for (Salas sala : salas) {
-                    LocalTime hora = LocalTime.of(10, 0); // El cine abre a las 10:00
-                    LocalTime limite = LocalTime.of(23, 45); // Última función inicia a más tardar a esta hora
+                    LocalTime hora = LocalTime.of(10, 0);
+                    LocalTime limite = LocalTime.of(23, 45);
+    
                     while (hora.plusMinutes(duracion + 15).isBefore(limite)) {
                         LocalTime horaFin = hora.plusMinutes(duracion);
+    
                         Salas_peliculas sp = new Salas_peliculas(
-                            em.getNombre(),
                             Date.valueOf(fecha),
                             Date.valueOf(fecha),
                             Time.valueOf(hora),
                             Time.valueOf(horaFin),
                             sala.getId(),
-                            em.getId(),
-                            0 // id_entrada si no lo usas aún
+                            idPelicula
                         );
+    
                         dao.agregarSalaPelicula(sp);
-                        hora = horaFin.plusMinutes(15); // Tiempo de limpieza
+                        hora = horaFin.plusMinutes(15); // margen de tiempo entre pelis
                     }
                 }
-            }    
+            }
         }
-        System.out.println("Funciones programadas automáticamente.");
+    
+        System.out.println(" Programación automática completada.");
     }
     
     public void eliminarSalasPeliculas(){
