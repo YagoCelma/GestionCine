9d8c1235c1cdbb78781c01aa292bdfd2eb9a2e0d
samuel <samuel1122000@gmail.com>
2025-05-02 18:20:16 +0200

    Login hecho

--- src/main/App.java
+++ src/main/App.java
@@ -10,6 +10,23 @@ import view.*;
 public class App {
 
     public static void main(String[] args) throws Exception {
+
+        ClienteView clienteView = new ClienteView();
+        EmpleadoView empleadoView = new EmpleadoView();
+        PeliculaView peliculasView = new PeliculaView();
+        SalaView salaView = new SalaView();
+        ProveedorView proveedorView = new ProveedorView();
+        InventarioPeliculaView inventarioPeliculaView = new InventarioPeliculaView();
+        EntradasView entradasView = new EntradasView();
+
+        Scanner sc = new Scanner(System.in);
+        int resp ; 
+        int opcion;
+        int opcion2;
+        String admin = "administrator";
+        String contraAdmin = "admin123";
+        String emp = "empleado";
+        String contraEmp = "empleado123";
         
     // Verificación de conexión inicial
         try (Connection conn = ConexionDB.getConnection()) {
@@ -23,27 +40,15 @@ public class App {
             System.out.println("Error al conectar a la base de datos: " + e.getMessage());
             return;
         }
-
-        ClienteView clienteView = new ClienteView();
-        EmpleadoView empleadoView = new EmpleadoView();
-        PeliculaView peliculasView = new PeliculaView();
-        SalaView salaView = new SalaView();
-
-        Scanner sc = new Scanner(System.in);
-        int resp ; 
-        int opcion;
-        String admin = "administrator";
-        String contraAdmin = "aws23@12jk";
-        String emp = "empleado";
-        String contraEmp = "añle.,45%,";
         
         do{
-            System.out.println("1. Bienvenido a CineMax");
+            System.out.println("\n Bienvenido a CineMax");
             System.out.println("                       ");
-            System.out.println("2. Iniciar sesion como administrador");
-            System.out.println("3. Iniciar sesion como empleado");
-            System.out.println("4. Salir");
+            System.out.println("1. Iniciar sesion como administrador");
+            System.out.println("2. Iniciar sesion como empleado");
+            System.out.println("3. Salir");
             resp = sc.nextInt();
+            sc.nextLine();
             
             switch(resp){
                 case 1 -> {
@@ -51,16 +56,21 @@ public class App {
                     String administrador = sc.nextLine();
                     System.out.println("Introduca la contraseña de administrador");
                     String contraAdministrador = sc.nextLine();
+
                     if(administrador.equals(admin) && contraAdministrador.equals(contraAdmin)){
                         do{
+                            System.out.println("                                            ");
                             System.out.println("Bienvenido al Gestor Cine como adiminstrador");
                             System.out.println("--------------------------------------------");
                             System.out.println("1. Cliente");
                             System.out.println("2. Empleado");
                             System.out.println("3. Pelicula");
                             System.out.println("4. Sala");
-                            //System.out.println("4. Sala"); Aqui podria ir para crear la cartelera de la semana
-                            System.out.println("5. Salir");
+                            System.out.println("5. Proveedores");
+                            System.out.println("6. Inventario peliculas");
+                            //System.out.println("7. Cartelera de la semana");
+                            System.out.println("8. Salir");
+                            System.out.print("Seleccione una opción: ");
                             opcion = sc.nextInt();
                             sc.nextLine();
                 
@@ -69,14 +79,15 @@ public class App {
                                 case 2-> empleadoView.gestionEmpleados();
                                 case 3-> peliculasView.menuPrincipal();
                                 case 4-> salaView.menuSala();
-                                case 5-> System.out.println("Hasta pronto...");
+                                case 5-> proveedorView.menuProveedor();
+                                case 6 -> inventarioPeliculaView.menuPrincipal();
+                                //case 7 -> Cartelera??
+                                case 8-> System.out.println("Hasta pronto...");
                                 default->System.out.println("Valor no valido, vuelve a intentarlo");
                             }
-                        }while(opcion != 5);
-
+                        }while(opcion != 8);
                     } else{
-                        System.out.println("Introduzca un usuario y contraseña validos");
-                        return;
+                        System.out.println("usuario o contraseña incorrectos");
                     }
                 }
 
@@ -85,41 +96,38 @@ public class App {
                     String empleado = sc.nextLine();
                     System.out.println("Introduca la contraseña de empleado");
                     String contraEmpleado = sc.nextLine();
+
                     if(empleado.equals(emp) && contraEmpleado.equals(contraEmp)){
-                        System.out.println("Bienvenido al cine como empleado");
-                        System.out.println("------------------------------- ");
-                        System.out.println("1. Cliente");
-                        System.out.println("2. Entradas");
-                        System.out.println("");
+                        do { 
+                            System.out.println("                                ");
+                            System.out.println("Bienvenido al cine como empleado");
+                            System.out.println("------------------------------- ");
+                            System.out.println("1. Cliente");
+                            System.out.println("2. Entradas");
+                            System.out.println("3. Consultar cartelera");
+                            System.out.println("4. Salir");
+                            System.out.print("Seleccione una opción: ");
+                            opcion2 = sc.nextInt();
+                            sc.nextLine();
 
+                            switch(opcion2){
+                                case 1 -> clienteView.menuCliente();
+                                case 2 -> entradasView.menuEntrada();
+                                //case 3 -> Cartelera
+                                case 4-> System.out.println("Hasta pronto...");
+                                default->System.out.println("Valor no valido, vuelve a intentarlo");
+                            }
+
+                        } while (opcion2 != 4);
                     }else {
-                        System.out.println("Introduzca un usuario y contraseña validos");
+                        System.out.println("usuario o contraseña incorrectos");
                     }
-
                 }
-            }  
-        }while(resp !=4);
 
-
-        do{
-            System.out.println("Bienvenido al Gestor Cine como adiminstrador");
-            System.out.println("-------------------------");
-            System.out.println("1. Cliente");
-            System.out.println("2. Empleado");
-            System.out.println("3. Pelicula");
-            System.out.println("4. Sala");
-            System.out.println("5. Salir");
-            opcion = sc.nextInt();
-            sc.nextLine();
-
-            switch(opcion){
-                case 1-> clienteView.menuCliente();
-                case 2-> empleadoView.gestionEmpleados();
-                case 3-> peliculasView.menuPrincipal();
-                case 4-> salaView.menuSala();
-                case 5-> System.out.println("Hasta pronto...");
-                default->System.out.println("Valor no valido, vuelve a intentarlo");
-            }
-        }while(opcion != 5);
+                case 3 -> System.out.println("Gracias por usar CineMax. ¡Hasta la próxima!");
+                default -> System.out.println("Opción inválida. Intente nuevamente.");
+            } 
+            
+        }while(resp !=3);
     }
 }
