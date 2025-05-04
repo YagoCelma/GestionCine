b242d9fe7efe004e962f64b9b3b422124f9449d3
samuel <samuel1122000@gmail.com>
2025-05-04 12:25:44 +0200

    carteleraView

---
+++ src/dao/CarteleraDAO.java
@@ -0,0 +1,20 @@
+package dao;
+import java.util.ArrayList;
+import model.Cartelera;
+
+public class CarteleraDAO {
+
+    public void añadirCartelera(Cartelera cartelera){
+
+    }
+
+    public boolean eliminarPeliCartelera(String titulo){
+        return true;
+    }
+
+    public ArrayList<Cartelera> mostrarCartelera(){
+        ArrayList<Cartelera> cartelera = new ArrayList<>();
+        return cartelera;
+    }
+    
+}
---
+++ src/view/CarteleraView.java
@@ -0,0 +1,81 @@
+package view;
+
+import dao.CarteleraDAO;
+import dao.PeliculaDAO;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import model.Cartelera;
+import model.Pelicula;
+
+public class CarteleraView {
+
+    private final Scanner sc = new Scanner(System.in);
+    private final PeliculaDAO peliculaDAO = new PeliculaDAO();
+    private final CarteleraDAO carteleraDAO = new CarteleraDAO();
+    
+    public void gestionCartelera(){
+
+        int opcion = 0;
+
+        do { 
+
+            System.out.println("\n Bienvenido a la cartelera ");
+            System.out.println("1. Crear cartelera ");
+            System.out.println("2. Eliminar pelicula de la cartelera ");
+            System.out.println("3. Mostrar cartelera ");
+            System.out.println("4. Actualizar cartelera ");
+            System.out.println("5. Salir ");
+            System.out.println("Seleccione una opcion: ");
+            opcion = sc.nextInt();
+
+            switch(opcion){
+                case 1 -> this.crearCartelera();
+                case 2 -> this.eliminarPeliCartelera();
+                case 3 -> this.mostrarCartelera();
+            }
+            
+        } while (opcion != 5);
+    }
+
+    public void crearCartelera() {
+        String resp = "si";
+        try {
+            do { 
+                System.out.println("\n ¿Qué película desea añadir?");
+                String titulo = sc.nextLine();
+                Pelicula peli = peliculaDAO.mostrarPeliculaByTitulo(titulo);
+                if (peli != null) {
+                    System.out.println("Ingrese una película que sea propiedad del cine");
+                } else {
+                    Cartelera cartelera = new Cartelera(titulo);
+                    carteleraDAO.añadirCartelera(cartelera);
+                    System.out.println("Película añadida correctamente");
+                }
+                System.out.println("Desea seguir añadiendo peliculas? (si/no)"); 
+                resp = sc.nextLine();
+            } while (resp.equalsIgnoreCase("si"));
+        } catch (SQLException e) {
+            System.out.println("Error al buscar la película: " + e.getMessage());
+        }
+    }
+
+    public void eliminarPeliCartelera(){
+        System.out.println("Ingrese el titulo de la pelicula a eliminar");
+        String titulo = sc.nextLine();
+        if(carteleraDAO.eliminarPeliCartelera(titulo)){
+            System.out.println("Pelicula eliminada de la cartelera");
+        }else{
+            System.out.println("No se encontro ninguna pelicula con ese titulo");
+        }
+    }
+
+    public void mostrarCartelera(){
+        ArrayList<Cartelera> cartelera = carteleraDAO.mostrarCartelera();
+        for(Cartelera cartel : cartelera){
+            System.out.println(cartel);
+        }
+        
+    }
+    
+}
